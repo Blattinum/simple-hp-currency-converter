b5752161c40f2fdf5ccb7249ff7fa456c830b802
Collin Blatt <collin.blatt@gmail.com>
2017-11-02 17:48:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var galleonsDollarValue, sicklesDollarValue, knutsDollarValue;
+
+if(document.getElementById("reddit_Method").checked) {
+	galleonsDollarValue = 25;
+	sicklesDollarValue = 1.50;
+	knutsDollarValue = 0.05;
+}else if(document.getElementById("wikia_Method").checked) {
+	galleonsDollarValue = 7.35;
+	sicklesDollarValue = 0.46;
+	knutsDollarValue = 0.02;
+}
+document.getElementById("btnCalc").addEventListener("click", function(){
+    document.getElementById("lblResult").innerHTML = "Hello World";
+});
+
+

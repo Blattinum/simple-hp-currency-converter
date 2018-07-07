3e0a9262e633ae65c8dc221c9017b19eb6a0b18a
Joseph Wildey <joewildey@outlook.com>
2018-07-07 04:18:24 -0400

    Update script.js
    Instead of treating each individual conversion factor as its own variable, I'm treating the amount the user has of each amount of HP currency as its own variable and modifying what they have by the constant conversion factor. If I have not broken anything in the process simple arithmetic would return the final conversion. Then it's a matter of writing it to the site.

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 					  Version 3, 29 June 2007
 */
 
-var usrGalDollarValue;
-var usrSicklDollarValue;
-var usrKnutsDollarValue;
+var usrGalleonsDollarValue = document.getElementById("lblGalleons");
+var usrSicklesDollarValue = document.getElementById("lblSickles");
+var usrKnutsDollarValue = document.getElementById("lblKnuts");
 
 if(document.getElementById("reddit_Method").checked) {
-	galleonsDollarValue = 25;
-	sicklesDollarValue = 1.50;
-	knutsDollarValue = 0.05;
+	usrGalleonsDollarValue *= 25;
+	usrSicklesDollarValue *= 1.50;
+	usrKnutsDollarValue *= 0.05;
 }else if(document.getElementById("wikia_Method").checked) {
-	galleonsDollarValue = 7.35;
-	sicklesDollarValue = 0.46;
-	knutsDollarValue = 0.02;
+	usrGalleonsDollarValue *= 7.35;
+	usrSicklesDollarValue *= 0.46;
+	usrKnutsDollarValue *= 0.02;
 }
 document.getElementById("btnCalc").addEventListener("click", function(){
     document.getElementById("lblResult").innerHTML = "Hello World";

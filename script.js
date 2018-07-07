dfb1adaf8fb75188f587463db99381248a7c9c9d
Joseph Wildey <joewildey@outlook.com>
2018-07-07 04:53:41 -0400

    Update script.js
    The easiest way I can think of to handle this is to ultimately create a function with a return value. There is now a potential return value for the potential future function.

--- script.js
+++ script.js
@@ -5,18 +5,19 @@
 var usrGalleonsDollarValue = document.getElementById("lblGalleons");
 var usrSicklesDollarValue = document.getElementById("lblSickles");
 var usrKnutsDollarValue = document.getElementById("lblKnuts");
+var usrDollars;
 
 if(document.getElementById("reddit_Method").checked) {
 	usrGalleonsDollarValue *= 25;
 	usrSicklesDollarValue *= 1.50;
 	usrKnutsDollarValue *= 0.05;
+	usrDollars = usrGalleonsDollarValue + usrSicklesDollarValue + usrKnutsDollarValue; 
 }else if(document.getElementById("wikia_Method").checked) {
 	usrGalleonsDollarValue *= 7.35;
 	usrSicklesDollarValue *= 0.46;
 	usrKnutsDollarValue *= 0.02;
+	usrDollars = usrGalleonsDollarValue + usrSicklesDollarValue + usrKnutsDollarValue;
 }
 document.getElementById("btnCalc").addEventListener("click", function(){
     document.getElementById("lblResult").innerHTML = "Hello World";
 });
-
-

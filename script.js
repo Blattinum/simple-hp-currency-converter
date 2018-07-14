9330cf9e4567a8edf381ada0582aa1726fc2ac85
Joseph Wildey <joewildey@outlook.com>
2018-07-13 20:09:30 -0400

    Update script.js
    This should define a function and assign that function to a listener that will be activated when that listener is interacted with. Further, the function itself is given a return value so that something will happen.

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ var usrSicklesDollarValue = document.getElementById('lblSickles').innerHTML;
 var usrKnutsDollarValue = document.getElementById('lblKnuts').innerHTML;
 var usrDollars;
 
-if(document.getElementById("reddit_Method").checked) {
-	usrGalleonsDollarValue *= 25;
-	usrSicklesDollarValue *= 1.50;
-	usrKnutsDollarValue *= 0.05;
-	usrDollars = usrGalleonsDollarValue + usrSicklesDollarValue + usrKnutsDollarValue; 
-}else if(document.getElementById("wikia_Method").checked) {
-	usrGalleonsDollarValue *= 7.35;
-	usrSicklesDollarValue *= 0.46;
-	usrKnutsDollarValue *= 0.02;
-	usrDollars = usrGalleonsDollarValue + usrSicklesDollarValue + usrKnutsDollarValue;
+function ConversionCalc () {
+	if(document.getElementById("reddit_Method").checked) {
+		usrGalleonsDollarValue *= 25;
+		usrSicklesDollarValue *= 1.50;
+		usrKnutsDollarValue *= 0.05;
+		usrDollars = usrGalleonsDollarValue + usrSicklesDollarValue + usrKnutsDollarValue; 
+	}else if(document.getElementById("wikia_Method").checked) {
+		usrGalleonsDollarValue *= 7.35;
+		usrSicklesDollarValue *= 0.46;
+		usrKnutsDollarValue *= 0.02;
+		usrDollars = usrGalleonsDollarValue + usrSicklesDollarValue + usrKnutsDollarValue;
+	}
+	return usrDollars;
 }
-document.getElementById("btnCalc").addEventListener("click", function(){
+
+document.getElementById("btnCalc").addEventListener("click", ConversionCalc (){
     document.getElementById("lblResult").innerHTML = "Hello World";
 });
